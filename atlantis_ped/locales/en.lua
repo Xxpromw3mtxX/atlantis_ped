5096941ad1dc445658401e0145e13ce7e866ba70
Francesco Metta <fra.metta@hotmail.it>
2022-01-01 14:20:31 +0100

    added english localization file

---
+++ atlantis_ped/locales/en.lua
@@ -0,0 +1,9 @@
+Locales['it'] = {
+    ['ped_changed'] = 'The ped %s was set correctly!',
+    ['ped_reset'] = 'The ped was restored correctly!',
+    ['not_staff'] = 'You are not part of the staff!',
+    ['ped_menu'] = 'Ped Menu',
+    ['menu_suggestion'] = 'Opens the ped selection menu',
+    ['female'] = 'Female Peds',
+    ['male'] = 'Male Peds'
+}
